860f41579be3fd81b35286b902f2bea0a6d905c3
Deepak jr <deepakjeganathan@gmail.com>
2020-08-24 20:59:09 +0100

    Revert "power: oneplus: Optimize coding"
    This reverts commit 9f4a892aa8b590c99f9069404d1665d78f2aed87.

--- drivers/power/bq27541_fuelgauger.c
+++ drivers/power/bq27541_fuelgauger.c
@@ -197,8 +197,7 @@ static int bq27541_battery_temperature(struct bq27541_device_info *di)
 {
 	int ret;
 	int temp = 0;
-	int error_temp;
-	static int count;
+	static int count = 0;
 
 	/* Add for get right soc when sleep long time */
 	if (atomic_read(&di->suspended) == 1)
@@ -221,8 +220,7 @@ static int bq27541_battery_temperature(struct bq27541_device_info *di)
 				 */
 				di->temp_pre =
 				-400 - ZERO_DEGREE_CELSIUS_IN_TENTH_KELVIN;
-				error_temp = -400;
-				return error_temp;
+				return -400;
 			} else {
 				return di->temp_pre +
 				ZERO_DEGREE_CELSIUS_IN_TENTH_KELVIN;
@@ -295,7 +293,7 @@ static int bq27541_i2c_txsubcmd(u8 reg, unsigned short subcmd,
 	return 0;
 }
 
-static bool bq27541_regist_done;
+static bool bq27541_regist_done = false;
 static bool check_4p4v_bat_present(struct bq27541_device_info *di)
 {
 	int flags = 0, ret = 0;
@@ -463,8 +461,8 @@ static int fg_soc_calibrate(struct bq27541_device_info *di, int soc)
 	union power_supply_propval ret = {0,};
 	unsigned int soc_calib;
 	unsigned long soc_current_time, time_last;
-	static bool first_enter;
-	static int charging_status, charging_status_pre;
+	static bool first_enter = false;
+	static int charging_status, charging_status_pre = 0;
 	bool chg_done;
 	int temp_region, vbat_mv, ibat_ma, soc_load, soc_temp, counter_temp = 0;
 
@@ -1182,11 +1180,9 @@ static ssize_t bq27541_write_stdcmd(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
 	ssize_t ret = strnlen(buf, PAGE_SIZE);
-	int cmd, rc;
+	int cmd;
 
-	rc = kstrtou32(buf, 0, &cmd);
-	if (rc != 1)
-		pr_err("%s,scanf error\n");
+	sscanf(buf, "%x", &cmd);
 	reg = cmd;
 	return ret;
 }
@@ -1225,18 +1221,16 @@ static ssize_t bq27541_write_subcmd(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
 	ssize_t ret = strnlen(buf, PAGE_SIZE);
-	int cmd, rc;
+	int cmd;
 
-	rc = kstrtou32(buf, 0, &cmd);
-	if (rc != 1)
-		pr_err("%s,scanf error\n");
+	sscanf(buf, "%x", &cmd);
 	subcmd = cmd;
 	return ret;
 }
 
-static DEVICE_ATTR(std_cmd, 0644, bq27541_read_stdcmd,
+static DEVICE_ATTR(std_cmd, S_IRUGO|S_IWUGO, bq27541_read_stdcmd,
 		bq27541_write_stdcmd);
-static DEVICE_ATTR(sub_cmd, 0644, bq27541_read_subcmd,
+static DEVICE_ATTR(sub_cmd, S_IRUGO|S_IWUGO, bq27541_read_subcmd,
 		bq27541_write_subcmd);
 static struct attribute *fs_attrs[] = {
 	&dev_attr_std_cmd.attr,
@@ -1312,6 +1306,7 @@ static int bq27541_battery_probe(struct i2c_client *client,
 
 	di = kzalloc(sizeof(*di), GFP_KERNEL);
 	if (!di) {
+		pr_err("failed to allocate device info data\n");
 		retval = -ENOMEM;
 		goto batt_failed_1;
 	}
@@ -1319,6 +1314,7 @@ static int bq27541_battery_probe(struct i2c_client *client,
 
 	bus = kzalloc(sizeof(*bus), GFP_KERNEL);
 	if (!bus) {
+		pr_err("failed to allocate access method data\n");
 		retval = -ENOMEM;
 		goto batt_failed_2;
 	}
@@ -1427,7 +1423,7 @@ static int bq27541_battery_suspend(struct i2c_client *client,
 }
 
 /* 1 minute */
-#define RESUME_TIME	60
+#define RESUME_TIME	1*60
 
 static int bq27541_battery_resume(struct i2c_client *client)
 {
@@ -1497,7 +1493,7 @@ static int __init bq27541_battery_init(void)
 
 	ret = i2c_add_driver(&bq27541_battery_driver);
 	if (ret)
-		pr_err("Unable to register BQ27541 driver\n");
+		printk(KERN_ERR "Unable to register BQ27541 driver\n");
 
 	return ret;
 }
--- drivers/power/oem_external_fg.h
+++ drivers/power/oem_external_fg.h
@@ -79,7 +79,7 @@ struct notify_usb_enumeration_status {
 	int (*notify_usb_enumeration)(int status);
 };
 
-enum temp_region_type {
+typedef enum {
 	BATT_TEMP_COLD = 0,
 	BATT_TEMP_LITTLE_COLD,
 	BATT_TEMP_COOL,
@@ -89,7 +89,7 @@ enum temp_region_type {
 	BATT_TEMP_WARM,
 	BATT_TEMP_HOT,
 	BATT_TEMP_INVALID,
-};
+} temp_region_type;
 
 void regsister_notify_usb_enumeration_status(
 	struct notify_usb_enumeration_status *event);
@@ -113,13 +113,10 @@ extern int load_soc(void);
 extern void backup_soc_ex(int soc);
 extern void clean_backup_soc_ex(void);
 
-/* Remove Static & extern to qpnp-smbcharger.c */
-extern void switch_mode_to_normal(void);
-
 /* From oneplus_fastchg.c */
 extern void msm_cpuidle_set_sleep_disable(bool disable);
 
 /* add for dash adapter update */
-extern bool dash_adapter_update_is_tx_gpio(unsigned int gpio_num);
-extern bool dash_adapter_update_is_rx_gpio(unsigned int gpio_num);
+extern bool dash_adapter_update_is_tx_gpio(unsigned long gpio_num);
+extern bool dash_adapter_update_is_rx_gpio(unsigned long gpio_num);
 #endif
